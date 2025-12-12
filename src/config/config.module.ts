9b527bff96d13ce43ecbd10b8664e4a286ead5a2
Dev-Stefano <stefano_dev@outlook.com>
2025-12-12 15:52:46 -0500

    Create config.module.ts

---
+++ src/config/config.module.ts
@@ -0,0 +1,11 @@
+import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
+
+@Module({
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true, // makes env vars available everywhere
+    }),
+  ],
+})
+export class AppConfigModule {}
