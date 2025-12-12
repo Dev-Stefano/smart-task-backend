cecdaada73b13de86b53ebc68d148dfc688e20ad
Dev-Stefano <stefano_dev@outlook.com>
2025-12-12 14:54:23 -0500

    Create database.module.ts

---
+++ src/database/database.module.ts
@@ -0,0 +1,9 @@
+import { Module } from '@nestjs/common';
+import { MongooseModule } from '@nestjs/mongoose';
+
+@Module({
+  imports: [
+    MongooseModule.forRoot(process.env.MONGO_URI), // use your Atlas URI here
+  ],
+})
+export class DatabaseModule {}
