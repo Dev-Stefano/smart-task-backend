765afddd9e7768206c2110e1b2573f50e1d46a06
Dev-Stefano <stefano_dev@outlook.com>
2025-12-12 15:45:17 -0500

    Create auth.module.ts

---
+++ src/auth/auth.module.ts
@@ -0,0 +1,10 @@
+// Auth module for login/register
+import { Module } from '@nestjs/common';
+import { AuthController } from './auth.controller';
+import { AuthService } from './auth.service';
+
+@Module({
+  controllers: [AuthController],
+  providers: [AuthService],
+})
+export class AuthModule {}
